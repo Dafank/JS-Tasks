5d130dba65104147053e999e92b73bdbd6f14e7b
Dafank <fentik9814@gmail.com>
2020-04-02 01:17:14 +0300

    Add cycle works

--- First task/task.js
+++ First task/task.js
@@ -1,20 +1,39 @@
 "use strict";
 
-let money = prompt("What is your budget for the month?"),
-    time = prompt("Enter a date in the format YYYY-MM-DD"),
-    item = prompt("Введите обязательную статью расходов в этом месяце"),
-    cost = prompt("Во сколько обойдется?");
+let money = +prompt("What is your budget for the month?"),
+    time = prompt("Enter a date in the format YYYY-MM-DD");
 
 let appData = {
     budget: money,
     data: time,
-    expenses: {
-        item: cost
-    },
+    expenses: {},
     optionalExpenses: null,
     income: null,
     saving: false
 };
 
-alert("Your budget for one day - " + money / 30);
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = +prompt("Во сколько обойдется?");
 
+    if (typeof (b) != null && typeof (a) != null &&
+        a != '' && b != '' && a.length < 50 && !Number.isNaN(b)) {
+        appData.expenses[a] = b;
+    } else {
+        i--;
+    }
+}
+
+appData.moneyPerDay = Math.floor(appData.budget / 30);
+
+alert("Your budget for one day - " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 200) {
+    alert("Minimum level of prosperity");
+} else if (appData.moneyPerDay > 200 && appData.moneyPerDay < 2000) {
+    alert("Average level of prosperity");
+} else if (appData.moneyPerDay > 2000) {
+    alert("High level of prosperity");
+} else {
+    alert("Something going wrong!")
+}
---
+++ Test/test1.js
@@ -0,0 +1,22 @@
+// let x=5;
+// console.log(x++);//5
+
+// console.log([]+false-null+true);// NaN
+
+// let y=1,
+//     x=y=2;
+//     console.log(x);//2
+
+// console.log([]+1+2);//12?
+
+// console.log("1"[0]);//1?
+
+// console.log(2 && 1 && null && 0 && undefined);//null?
+
+// console.log(null || 2 && 3 || 4 );//3?
+
+// console.log(+"Infinity");//Infinity
+
+// console.log("йожик">"яблуко");//false
+
+// console.log(0 || "" || 2 || undefined || true || falsе );//2
