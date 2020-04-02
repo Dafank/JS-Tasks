0d53b2c4d8b968806834a5840aeec6a962c1fb93
Dafank <fentik9814@gmail.com>
2020-04-02 23:00:59 +0300

    Pass fourth test

---
+++ Test/test4.js
@@ -0,0 +1,38 @@
+'use strict'
+
+// First task
+
+// let answer = prompt("What`s official JavaScript name?");
+
+// if (answer == "ECMA") {
+//     alert("Correct!");
+// } else {
+//     alert("You don`t know? \"ECMAScript!\"");
+// }
+
+// Second task
+
+// let num = prompt("Enter some number: ");
+
+// if (num > 0) {
+//     alert(1);
+// } else if (num < 0) {
+//     alert(-1);
+// } else {
+//     alert(0);
+// }
+
+// Third task
+
+// let a, b;
+// a = b = 1; // or 2
+// let result = (a + b < 4) ? "Little" : "Much";
+// console.log(result);
+
+// Fourth task
+
+// let login = prompt("Enter your login:"),
+//     message = (login == "Employee") ? "Hi" :
+//     (login == "Director") ? "Hello" :
+//     (login == '') ? "No login" : '';
+// alert(message);
